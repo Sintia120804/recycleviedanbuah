49600294d2650dea06d886ae01b45e6e292080b5
Sintia120804 <tia877805@gmail.com>
2024-10-14 17:51:48 +0700

    sintia

---
+++ app/src/main/java/com/sintia/loginpage/ListView.kt
@@ -0,0 +1,57 @@
+package com.sintia.loginpage
+
+import android.annotation.SuppressLint
+import   android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.sintia.loginpage.R
+
+class ListView : AppCompatActivity() {
+    private lateinit var lv_data : ListView
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_view)
+
+        lv_data = findViewById(R.id.lv_data)
+
+        //Listview
+        //widget listview --> Array adaptar --> data source (array/list/database)
+
+        //bikin array list
+        val namaHewan = listOf(
+            "gajah",
+            "kuda",
+            "beruang",
+            "panda",
+            "ular",
+            "kucing",
+            "sapi",
+            "kerbau",
+            "ikan"
+        )
+
+        //memasukkan data array ini ke dalam adater
+        //android layout --> layout bawaan untuk di adapter
+        lv_data.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, namaHewan)
+
+        //untuk bisa di klik dari list
+        lv_data.setOnItemClickListener {
+                parent, view, position, id ->
+            Toast.makeText(this, "anda memilih : ${namaHewan[position]}",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/sintia/loginpage/adapter/BukuAdapter.kt
@@ -0,0 +1,47 @@
+package com.sintia.loginpage.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+import com.sintia.loginpage.R
+import com.sintia.loginpage.model.ModelBuku
+
+
+data class BukuAdapter(private val buku: List<ModelBuku>) :
+    RecyclerView.Adapter<BukuAdapter.BukuAdapterHolder>() {
+
+    //view holder yang nanti gunakan untuk menyimpan referensi widget item layout
+    class BukuAdapterHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtJudul: TextView = view.findViewById(R.id.txtJudul)
+        val txtPenulis: TextView = view.findViewById(R.id.txtPenulis)
+    }
+
+    class MyViewHolder {
+
+    }
+
+    //untuk layout recycle
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BukuAdapter.BukuAdapterHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_custom_buah, parent, false)
+        return BukuAdapterHolder(view)
+    }
+
+    //diatur untuk pengaturan data item pada setiap list dari recycle view
+    override fun onBindViewHolder(holder: BukuAdapter.BukuAdapterHolder, position: Int) {
+        holder.txtJudul.text = buku[position].judul
+        holder.txtPenulis.text = buku[position].penulis
+    }
+
+    override fun getItemCount(): Int {
+        return buku.size
+    }
+
+}
+
